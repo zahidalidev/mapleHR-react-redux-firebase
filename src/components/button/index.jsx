ea9852f247a1fffbcf917e353079d0c482ac343a
zahidalidev <m.zahidalidev@gmail.com>
2022-03-02 16:31:14 +0500

    return removed

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types'
 
 import 'components/button/styles.css'
 
-const Button = ({ btnName, backgroundColor = '#00b8b0' }) => {
-  return (
-    <div className='sign-in-form-button'>
-      <MuiButton
-        style={{ backgroundColor }}
-        className='landing-page-button'
-        type='submit'
-        variant='contained'
-        data-flag={btnName}
-      >
-        {btnName}
-      </MuiButton>
-    </div>
-  )
-}
+const Button = ({ btnName, backgroundColor = '#00b8b0' }) => (
+  <div className='sign-in-form-button'>
+    <MuiButton
+      style={{ backgroundColor }}
+      className='landing-page-button'
+      type='submit'
+      variant='contained'
+      data-flag={btnName}
+    >
+      {btnName}
+    </MuiButton>
+  </div>
+)
 
 Button.propTypes = {
   btnName: PropTypes.string.isRequired,
--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -4,21 +4,19 @@ import PropTypes from 'prop-types'
 
 import 'components/pagination/styles.css'
 
-const CusPagination = ({ pageCount, handlePageClick }) => {
-  return (
-    <div className='pagination-wrapper'>
-      <Stack spacing={2}>
-        <Pagination
-          count={pageCount}
-          onChange={handlePageClick}
-          renderItem={item => (
-            <PaginationItem components={{ previous: ArrowBack, next: ArrowForward }} {...item} />
-          )}
-        />
-      </Stack>
-    </div>
-  )
-}
+const CusPagination = ({ pageCount, handlePageClick }) => (
+  <div className='pagination-wrapper'>
+    <Stack spacing={2}>
+      <Pagination
+        count={pageCount}
+        onChange={handlePageClick}
+        renderItem={item => (
+          <PaginationItem components={{ previous: ArrowBack, next: ArrowForward }} {...item} />
+        )}
+      />
+    </Stack>
+  </div>
+)
 
 CusPagination.propTypes = {
   pageCount: PropTypes.number.isRequired,
